2b9f408fe4c041024577ca9a20904e5a9859c23c
hyewon Lee <0303hyewon@gmail.com>
2025-04-14 19:40:14 +0900

    first

---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+// components/Header.jsx
+import React from 'react';
+import '../styles/Header.css';
+import { useNavigate } from 'react-router-dom';
+
+function Header() {
+  const navigate = useNavigate();
+
+  const handleSignInClick = () => {
+    if (window.location.pathname === '/signin') {
+      //다시 sign in 버튼을 눌렀을 때 새로고침 됨
+      navigate(0);
+    } else {
+      navigate('/signin');
+    }
+  };
+
+  return (
+    <header className="custom-header">
+      <div className="left-section">
+      <a href="/home" className="maintext-link">PLAY with ME</a>
+      </div>
+      <div className="center-section">
+        <span>(별명)님 환영합니다!</span>
+        <a href="/mypage" className="text-link">마이페이지</a>
+        <a href="/settings" className="text-link">설정</a>
+      </div>
+      <div className="right-section">
+      <button className="sign-in" onClick={handleSignInClick}>Sign in</button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/main/java/com/example/pwm/service/HostService.java
+++
@@ -1,8 +0,0 @@
-package com.example.pwm.service;
-
-
-import com.example.pwm.controller.dto.JoinRequest;
-
-public interface HostService {
-    String join(JoinRequest joinRequest);
-}
